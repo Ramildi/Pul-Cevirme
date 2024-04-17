26cc6e81bcab3133f8e68eeb70ca2581a4dcc7d2
unknown <ramil.mhrmf@gmail.com>
2024-04-17 11:14:06 +0400

    commit

--- apps.js
+++ apps.js
@@ -2,8 +2,8 @@ function Cevir() {
     var manat = parseFloat(document.getElementById('manat').value);
     var dolarNetice = 1.70;
     var euroNetice = 1.81;
-    var dolar = manat * dolarNetice;
-    var euro = manat * euroNetice;
+    var dolar = manat / dolarNetice;
+    var euro = manat / euroNetice;
 
     document.getElementById('dolarCavab').innerText = dolar.toFixed(2) + " $";
     document.getElementById('euroCavab').innerText = euro.toFixed(2) + " €";
